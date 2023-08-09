9173f3d0d644c7152e9a669c0262b2538557eeb0
MirelaScalabrini <90942816+MirelaScalabrini@users.noreply.github.com>
2023-08-09 13:57:46 +0000

    (09/08) configuração inicial do projeto

---
+++ back-end/src/app.js
@@ -0,0 +1,21 @@
+import express, { json, urlencoded } from "express";
+import cookieParser from "cookie-parser";
+import logger from "morgan";
+
+import indexRouter from "./routes/index.js";
+import usersRouter from "./routes/users.js";
+
+// importa o client do prisma para fazer a conexão com o Banco de dados
+import prisma from "./database/client.js";
+
+const app = express();
+
+app.use(logger("dev"));
+app.use(json());
+app.use(urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+
+export default app;
