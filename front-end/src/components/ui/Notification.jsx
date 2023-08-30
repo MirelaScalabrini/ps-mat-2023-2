ae11e3b13255c7bfbc518e3c438bacf95ec0267e
MirelaScalabrini <90942816+MirelaScalabrini@users.noreply.github.com>
2023-08-30 17:22:46 +0000

    (30/08) Continuação do cadastro de clientes

--- front-end/src/components/ui/Notification.jsx
+++ front-end/src/components/ui/Notification.jsx
@@ -6,20 +6,26 @@ const Alert = React.forwardRef(function Alert(props, ref) {
   return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
 });
 
-export default function Notification({show = false, message, severity = 'success', timeout = 4000, onClose}) {
-const handleClose = (event, reason) => {
+export default function Notification({
+    show = false, 
+    message, 
+    severity = 'success',
+    timeout = 4000,
+    onClose
+}) {
+
+  const handleClose = (event, reason) => {
     if (reason === 'clickaway') {
       return;
     }
-    if(typeof onClose === 'function') onClose(event,reason)
+    if(typeof onClose === 'function') onClose(event, reason)
   };
 
   return (
-      <Snackbar open={show} autoHideDuration={timeout} onClose={handleClose}>
-        
-        <Alert onClose={handleClose} severity="success" sx={{ width: '100%' }}>
-          This is a success message!
-        </Alert>
-      </Snackbar>
+    <Snackbar open={show} autoHideDuration={timeout} onClose={handleClose}>
+      <Alert onClose={handleClose} severity={severity} sx={{ width: '100%' }}>
+        { message }
+      </Alert>
+    </Snackbar>
   );
 }
--- front-end/src/pages/CustomersForm.jsx
+++ front-end/src/pages/CustomersForm.jsx
@@ -3,7 +3,7 @@ import Typography from '@mui/material/Typography'
 import TextField from '@mui/material/TextField'
 import MenuItem from '@mui/material/MenuItem'
 import Button from '@mui/material/Button'
-import Toolbar  from '@mui/material/Toolbar'
+import Toolbar from '@mui/material/Toolbar'
 import Box from '@mui/material/Box'
 import myfetch from '../utils/myfetch'
 import Waiting from '../components/ui/Waiting'
@@ -15,7 +15,7 @@ export default function CustomersForm() {
   const navigate = useNavigate()
 
   const [state, setState] = React.useState({
-    customer: {},     // Objeto vazio
+    customer: {},    // Objeto vazio
     showWaiting: false,
     notification: {
       show: false,
@@ -26,51 +26,64 @@ export default function CustomersForm() {
 
   const {
     customer,
-    showWaiting, 
+    showWaiting,
     notification
   } = state
 
   const states = [
-    { label: 'Distrito Federal', value: 'DF'},
-    { label: 'Espírito Santo', value: 'ES'},
-    { label: 'Goiás', value: 'GO'},
-    { label: 'Minas Gerais', value: 'MG'},
-    { label: 'Paraná', value: 'PR'},
-    { label: 'Rio de Janeiro', value: 'RJ'},
-    { label: 'São Paulo', value: 'SP'}
+    { label: 'Distrito Federal', value: 'DF' },
+    { label: 'Espírito Santo', value: 'ES' },
+    { label: 'Goiás', value: 'GO' },
+    { label: 'Minas Gerais', value: 'MG' },
+    { label: 'Paraná', value: 'PR' },
+    { label: 'Rio de Janeiro', value: 'RJ' },
+    { label: 'São Paulo', value: 'SP' }
   ]
 
   function handleFieldChange(event) {
     console.log(event)
-    const newCustomer = {...customer}
-    newCustomer [event.target.name] = event.target.value
-    setState({...state, customer: newCustomer})
+    const newCustomer = { ...customer }
+    newCustomer[event.target.name] = event.target.value
+    setState({ ...state, customer: newCustomer })
   }
 
   async function handleFormSubmit(event) {
     setState({ ...state, showWaiting: true }) // Exibe o backdrop
-    event.preventDefault(false) // Evita o recarregamento da página
+    event.preventDefault(false)   // Evita o recarregamento da página
     try {
       const result = await myfetch.post('customer', customer)
-      setState({ ...state, showWaiting: false, notification:{ show: true, severity: 'success', message: 'Dados salvos com sucesso!' }}) // Esconde o backdrop
-      
+      setState({ ...state, 
+        showWaiting: false, // Esconde o backdrop
+        notification: {
+          show: true,
+          severity: 'success',
+          message: 'Dados salvos com sucesso.'
+        }  
+      })  
     }
     catch(error) {
-      setState({ ...state, showWaiting: false,  // Esconde o backdrop
-        notification:{ show: true, severity: 'error', message: 'ERRO:' + error.message } }) 
+      setState({ ...state, 
+        showWaiting: false, // Esconde o backdrop
+        notification: {
+          show: true,
+          severity: 'error',
+          message: 'ERRO: ' + error.message
+        } 
+      })  
     }
   }
 
   function handleNotificationClose() {
     const status = notification.severity
-    // fecha a barra de notificação 
-    setState({...state, notification: {
+    
+    // Fecha a barra de notificação
+    setState({...state, notification: { 
       show: false,
       severity: status,
       message: ''
     }})
 
-    // Volta para a página de listagem 
+    // Volta para a página de listagem
     if(status === 'success') navigate('/customers')
   }
 
@@ -79,10 +92,12 @@ export default function CustomersForm() {
 
       <Waiting show={showWaiting} />
 
-      <Notification show={notification.show} 
-      severity={notification.severity} 
-      message={notification.message} 
-      onClose={handleNotificationClose} />
+      <Notification
+        show={notification.show}
+        severity={notification.severity}
+        message={notification.message}
+        onClose={handleNotificationClose}
+      /> 
 
       <Typography variant="h1" sx={{ mb: '50px' }}>
         Cadastro de clientes
@@ -90,17 +105,97 @@ export default function CustomersForm() {
 
       <form onSubmit={handleFormSubmit}>
 
-      <Box className="form-fields">
+        <Box className="form-fields">
+        
+          <TextField 
+            id="name"
+            name="name" 
+            label="Nome completo" 
+            variant="filled"
+            required
+            fullWidth
+            value={customer.name}
+            onChange={handleFieldChange}
+          />
 
-        <TextField id="name" name="name" label="Nome completo" variant="filled" required fullWidth value={customer.name} onChange={handleFieldChange} />
-        <TextField id="ident_document" name="ident_document" label="CPF" variant="filled" required fullWidth value={customer.ident_document} onChange={handleFieldChange} />
-        <TextField id="birth_date" name="birth_date" label="Data de nascimento" variant="filled" fullWidth value={customer.birth_date} onChange={handleFieldChange} />
-        <TextField id="street_name" name="street_name" label="Logradouro (Rua, Av., etc.)" variant="filled" required fullWidth placeholder="Ex.: Rua Principal" value={customer.street_name} onChange={handleFieldChange} />
-        <TextField id="house_number" name="house_number" label="N°" variant="filled" required fullWidth value={customer.house_number} onChange={handleFieldChange} />
-        <TextField id="complements" name="complements" label="Complemento" variant="filled" fullWidth placeholder='Apto., bloco, casa, etc.' value={customer.complements} onChange={handleFieldChange} />
-        <TextField id="neighborhood" name="neighborhood" label="Bairro" variant="filled" required fullWidth value={customer.neighborhood} onChange={handleFieldChange} />
-        <TextField id="municipality" name="municipality" label="Município" variant="filled" required fullWidth value={customer.municipality} onChange={handleFieldChange} />
-        <TextField
+          <TextField 
+            id="ident_document"
+            name="ident_document" 
+            label="CPF" 
+            variant="filled"
+            required
+            fullWidth
+            value={customer.ident_document}
+            onChange={handleFieldChange}
+          />
+
+          <TextField 
+            id="birth_date"
+            name="birth_date" 
+            label="Data de nascimento" 
+            variant="filled"
+            fullWidth
+            value={customer.birth_date}
+            onChange={handleFieldChange}
+          />
+
+          <TextField 
+            id="street_name"
+            name="street_name" 
+            label="Logradouro (Rua, Av., etc.)" 
+            variant="filled"
+            required
+            fullWidth
+            placeholder="Ex.: Rua Principal"
+            value={customer.street_name}
+            onChange={handleFieldChange}
+          />
+
+          <TextField 
+            id="house_number"
+            name="house_number" 
+            label="Nº" 
+            variant="filled"
+            required
+            fullWidth
+            value={customer.house_number}
+            onChange={handleFieldChange}
+          />
+
+          <TextField 
+            id="complements"
+            name="complements" 
+            label="Complemento" 
+            variant="filled"
+            fullWidth
+            placeholder="Apto., bloco, casa, etc."
+            value={customer.complements}
+            onChange={handleFieldChange}
+          />
+
+          <TextField 
+            id="neighborhood"
+            name="neighborhood" 
+            label="Bairro" 
+            variant="filled"
+            required
+            fullWidth
+            value={customer.neighborhood}
+            onChange={handleFieldChange}
+          />
+          
+          <TextField 
+            id="municipality"
+            name="municipality" 
+            label="Município" 
+            variant="filled"
+            required
+            fullWidth
+            value={customer.municipality}
+            onChange={handleFieldChange}
+          />
+
+          <TextField
             id="state"
             name="state"
             select
@@ -110,7 +205,7 @@ export default function CustomersForm() {
             required
             value={customer.state}
             onChange={handleFieldChange}
-            >
+          >
             {states.map((option) => (
               <MenuItem key={option.value} value={option.value}>
                 {option.label}
@@ -118,20 +213,42 @@ export default function CustomersForm() {
             ))}
           </TextField>
 
-        <TextField id="phone" name="phone" label="Celular / Telefone de contato" variant="filled" required fullWidth value={customer.phone} onChange={handleFieldChange} />
-        <TextField id="email" name="email" label="E-mail" variant="filled" required fullWidth value={customer.email} onChange={handleFieldChange} />
+          <TextField 
+            id="phone"
+            name="phone" 
+            label="Celular / Telefone de contato" 
+            variant="filled"
+            required
+            fullWidth
+            value={customer.phone}
+            onChange={handleFieldChange}
+          />
 
-      </Box>
+          <TextField 
+            id="email"
+            name="email" 
+            label="E-mail" 
+            variant="filled"
+            required
+            fullWidth
+            value={customer.email}
+            onChange={handleFieldChange}
+          />
+          
+        </Box>
 
-      <Box sx={{ fontFamily: 'monospace' }}>
+        <Box sx={{ fontFamily: 'monospace' }}>
           { JSON.stringify(customer) }
-      </Box>
+        </Box>
 
-      <Toolbar sx={{ justifyContent: "space-around"}}>
-        <Button variant="contained" color="secondary" type="submit">Salvar</Button>
-        <Button variant="outlined">Voltar</Button>
-      </Toolbar>
+        <Toolbar sx={{ justifyContent: "space-around" }}>
+          <Button variant="contained" color="secondary" type="submit">Salvar</Button>
+          <Button variant="outlined">Voltar</Button>
+        </Toolbar>
+      
       </form>
     </>
+
+    
   )
 }
