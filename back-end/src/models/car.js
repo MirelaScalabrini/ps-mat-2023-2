436632adcc880343da8d2bec6028384eefd2f6c8
MirelaScalabrini <90942816+MirelaScalabrini@users.noreply.github.com>
2023-11-12 20:30:46 +0000

    (12/11) - Trabalho 2

---
+++ back-end/src/models/car.js
@@ -0,0 +1,63 @@
+import { z } from 'zod';
+
+const today = new Date();
+
+const Car = z.object({
+  brand:
+  z.string()
+  .min(1, { message: 'O comprimento mínimo é 1' })
+  .max(25, { message: 'O comprimento máximo é 25' }),
+
+  model: 
+  z.string()
+  .min(1, { message: 'O comprimento mínimo é 1' })
+  .max(25, { message: 'O comprimento máximo é 25' }),
+  
+  color: 
+  z.string()
+  .min(4, { message: 'O comprimento mínimo é 4' })
+  .max(20, { message: 'O comprimento máximo é 20' }),
+
+  year_manufacture: 
+  z.number()
+  .int()
+  .min(1940, { message: 'O ano mínimo é 1940' })
+  .max(2023, { message: 'O ano máximo é 2023' }),
+
+  imported: 
+  z.boolean(),
+
+  plates: 
+  z.string()
+    .refine((value) => /^[A-Z0-9-]{8}$/.test(value), {
+      message: 'As placas devem conter apenas letras maiúsculas e números (8 caracteres).'
+    }),
+
+  selling_date:
+    z.date()
+      .nullable()
+      .refine((date) => {
+        if (date === null) return true; // Permite valor nulo
+        return date <= today; // A data não pode ser posterior à data de hoje
+      }, { message: 'A data de venda não pode ser posterior à data de hoje.' }),
+
+      selling_price:
+      z.number()
+        .nullable()
+        .refine((value) => {
+          // Se a data de venda for nula, o preço também deve ser nulo
+          if (value === null) return true;
+          
+          // Agora, garantimos que `parent` não é `undefined` antes de acessar `parent.selling_date`
+          return value >= 2000 && (value !== null && value >= 2000);
+        }, {
+          message: 'O preço de venda deve ser nulo ou pelo menos R$2.000.'
+        }),
+
+  customer_id:
+  z.number()
+  .int()
+  .refine((value) => value >= 0 || value === null),
+});
+
+export default Car
--- front-end/src/pages/CarsForm.jsx
+++ front-end/src/pages/CarsForm.jsx
@@ -19,6 +19,8 @@ import { parseISO } from 'date-fns'
 import FormControlLabel from '@mui/material/FormControlLabel'
 import Switch from '@mui/material/Switch'
 import  InputAdornment  from '@mui/material/InputAdornment'
+import Car from '../models/car'
+import { ZodError } from 'zod'
 
 export default function CarsForm() {
 
@@ -46,7 +48,8 @@ export default function CarsForm() {
       message: ''
     },
     openDialog: false,
-    isFormModified: false
+    isFormModified: false,
+    validationErrors: {}
   })
 
   const {
@@ -55,7 +58,8 @@ export default function CarsForm() {
     showWaiting,
     notification,
     openDialog,
-    isFormModified 
+    isFormModified,
+    validationErrors 
   } = state
 
   const anos = []             
@@ -84,8 +88,13 @@ export default function CarsForm() {
       let car = carDefaults
       if(isUpdating){
         car = await myfetch.get(`car/${params.id}`)
-        car.selling_date = parseISO(car.selling_date)
+         // Verifica se selling_date é uma string não vazia antes de converter
+      if (typeof car.selling_date === 'string' && car.selling_date.trim() !== '') {
+        car.selling_date = parseISO(car.selling_date);
+      } else {
+        car.selling_date = null;
       }
+    }
 
       // Busca a listagem de clientes para preencher o componente de escolha
       let customers = await myfetch.get('customer')
@@ -110,13 +119,21 @@ export default function CarsForm() {
   function handleFieldChange(event) {
     console.log(event)
     const newCar = { ...car }
-    const value =
-      event.target.name === 'imported' /*Verifica se é importado*/ ? event.target.checked : event.target.name === 'year_manufacture' // Transforma a data de fabricação em inteiro
-      ? parseInt(event.target.value) : event.target.name === 'selling_price'
-      ? event.target.value.trim() === '' // Verifica se é uma string vazia se não 
-        ? null // Define como null ao editar e apagar o valor caso tenha preenchido
-        : event.target.value : event.target.value; 
-    newCar[event.target.name] = value;     
+    let value
+    if (event.target.name === 'imported') {
+      // Verifica se é importado
+      value = event.target.checked;
+    } else if (event.target.name === 'year_manufacture') {
+      // Transforma o ano de fabricação em inteiro
+      value = parseInt(event.target.value);
+    } else if (event.target.name === 'selling_price') {
+      // Verifica se é uma string vazia ou não é um número válido
+      value = isNaN(parseFloat(event.target.value)) ? null : parseFloat(event.target.value);
+    } else {
+      // Para outros campos, pega diretamente o valor
+      value = event.target.value;
+    }
+    newCar[event.target.name] = value    
     
     setState({ ...state, 
       car: newCar, 
@@ -128,6 +145,9 @@ export default function CarsForm() {
     setState({ ...state, showWaiting: true }) // Exibe o backdrop
     event.preventDefault(false)   // Evita o recarregamento da página
     try {
+      //chama a validação da biblioteca Zod
+      Car.parse(car)
+
       let result 
       if(car.id) result = await myfetch.put(`car/${car.id}`, car)
       else result = await myfetch.post('car', car)
@@ -137,17 +157,37 @@ export default function CarsForm() {
         notification: {
           show: true,
           severity: 'success',
-          message: 'Dados salvos com sucesso.'
+          message: 'Dados salvos com sucesso.',
+          validationErrors: {}
         }  
       })  
     }
     catch(error) {
-      setState({ ...state, 
+      if(error instanceof ZodError) {
+        console.error(error)
+
+        //Preenchendo os estados validationError para exibir os erros para o usuário
+        let valErrors = {}
+        for(let e of error.issues) valErrors[e.path[0]] = e.message
+        
+        setState({
+          ...state,
+          validationErrors: valErrors,
+          showWaiting: false, // Esconde o backdrop
+          notification: {
+            show: true,
+            severity: 'error',
+            message: 'ERRO: há campos inválidos no formulário.'
+          }
+        })
+      }
+      else setState({ ...state, 
         showWaiting: false, // Esconde o backdrop
         notification: {
           show: true,
           severity: 'error',
-          message: 'ERRO: ' + error.message
+          message: 'ERRO: ' + error.message,
+          validationErrors: {}
         } 
       })  
     }
@@ -225,6 +265,8 @@ export default function CarsForm() {
             value={car.brand}
             onChange={handleFieldChange}
             autoFocus
+            error={validationErrors?.brand}
+            helperText={validationErrors?.brand}
          />
 
           <TextField
@@ -236,6 +278,8 @@ export default function CarsForm() {
             fullWidth
             value={car.model}
             onChange={handleFieldChange}
+            error={validationErrors?.model}
+            helperText={validationErrors?.model}
           />
 
           <TextField 
@@ -247,6 +291,8 @@ export default function CarsForm() {
             fullWidth
             value={car.color}
             onChange={handleFieldChange}
+            error={validationErrors?.color}
+            helperText={validationErrors?.color}
           />
         
         <TextField
@@ -284,31 +330,40 @@ export default function CarsForm() {
             mask="aaa-9@99"
             formatChars={maskFormatChars}
             maskChar=" "
-            value={car.plates.toUpperCase()} /* Placas em maiúsculas*/
-            onChange={handleFieldChange}
+            value={car.plates.toUpperCase()}
+            onChange={(event) => handleFieldChange({ target: { name: 'plates', value: event.target.value } })}
           >
-            {
-              () => <TextField
+            {() => (
+              <TextField
                 id="plates"
                 name="plates"
                 label="Placa"
                 variant="filled"
                 required
                 fullWidth
+                error={validationErrors?.plates}
+                helperText={validationErrors?.plates}
               />
-            }
+            )}
           </InputMask>
 
           <LocalizationProvider dateAdapter={AdapterDateFns} adapterLocale={ptLocale}>
-            <DatePicker
-              label="Data de venda"
-              value={car.selling_date}
-              onChange={ value => 
-                handleFieldChange({ target: { name: 'selling_date', value } }) 
-              }
-              slotProps={{ textField: { variant: 'filled', fullWidth: true } }}
-            />
-          </LocalizationProvider>
+          <DatePicker
+            label="Data de venda"
+            value={car.selling_date}
+            onChange={(value) =>
+              handleFieldChange({ target: { name: 'selling_date', value } })
+            }
+            slotProps={{
+              textField: {
+                variant: 'filled',
+                fullWidth: true,
+                error: validationErrors?.selling_date,
+                helperText: validationErrors?.selling_date,
+              },
+            }}
+          />
+        </LocalizationProvider>
           
           <TextField
           id="selling_price"
@@ -320,6 +375,8 @@ export default function CarsForm() {
           type="number"
           value={car.selling_price !== null ? car.selling_price : ''}
           onChange={handleFieldChange}
+          error={validationErrors?.selling_price}
+          helperText={validationErrors?.selling_price}
         >
           </TextField>
 
